6a1e03b2d86f9fbf1901722e017ae14b6fb48cd3
d.djran <d.djran@gmail.com>
2017-01-06 15:29:06 +0800

    tools class of printing

---
+++ src/com/djran/commonUtils/Print.java
@@ -0,0 +1,17 @@
+package com.djran.commonUtils;
+
+/**
+ * Created by d.djran@gmail.com on 2017-01-06.
+ * 本工具类描述打印工具方法
+ */
+public class Print {
+    public static void print(Object obj){
+        System.out.println(obj);
+    }
+    public static void print(){
+        System.out.println();
+    }
+    public static void printb(Object obj){
+        System.out.print(obj);
+    }
+}
